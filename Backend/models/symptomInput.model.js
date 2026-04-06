f7e8b4b0e3ba502be932047bd579f3e7ea1b9993
vagabondraj <raviraj06112005@gmail.com>
2026-04-06 19:34:40 +0530

    Symptom model add ed

---
+++ Backend/models/symptomInput.model.js
@@ -0,0 +1,19 @@
+const symptomFields = [
+  "cough",
+  "fever",
+  "headache",
+  "vomiting",
+  "diarrhea",
+  "fatigue",
+  "body_pain",
+  "sore_throat",
+  "runny_nose",
+  "breathlessness",
+  "chest_pain",
+  "abdominal_pain",
+  "rash",
+  "joint_pain",
+  "loss_of_taste_smell",
+];
+
+module.exports = { symptomFields };
